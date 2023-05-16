90ecc9ffdcd13e8f6d4b5e6706e2aa7c79e697aa
Pooria Azami <pooriaazami@gmail.com>
2023-05-17 00:11:51 +0330

    writing the response to a file

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,19 @@
+use tokio::{fs::File, io::AsyncWriteExt, sync::mpsc::Receiver};
+
 #[tokio::main]
 async fn main() {
-
-println!("Hello, World")
-
+    println!("Hello, World")
 }
 
+async fn write_responce_to_file(mut rx: Receiver<String>) {
+    let mut output_file = File::create("output.txt")
+        .await
+        .expect("There was an error while opening the output file");
 
+    while let Some(html) = rx.recv().await {
+        output_file
+            .write_all(html.as_bytes())
+            .await
+            .expect("There was an error while writing to the output file");
+    }
+}
